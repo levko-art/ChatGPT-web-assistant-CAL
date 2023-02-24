762ce99164f01df54bad156180d1a139e865eb44
a.levkivskyi <a.levkivskyi@smartweb.com.ua>
2023-02-24 21:24:03 +0200

    connect to mysql db

--- app/white_guardian/white_guardian.py
+++ app/white_guardian/white_guardian.py
@@ -1,9 +1,9 @@
-import datetime
-
 from pymysql import connect
 
 from settings import DATABASES
 
+print('*'*40)
+print(DATABASES.DB_TOPICS.PORT.value)
 connection = connect(
         host=DATABASES.DB_TOPICS.HOST.value,
         user=DATABASES.DB_TOPICS.USERNAME.value,
